63f3fc4b299b8378a9e78f54249eb57114c990b9
bhnvx <xzt______@naver.com>
2021-05-11 01:42:41 +0900

    Node page test

--- app.js
+++ app.js
@@ -11,6 +11,34 @@ const indexRoute = require("./routes/index");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const Session = require('express-session');
+const flash = require('connect-flash');
+var MongoDBStore = require('connect-mongodb-session')(Session);
+
+app.use(flash());
+
+var store = new MongoDBStore({
+    url: url,
+    collection: 'sessions'
+});
+
+store.on('error', function(error){
+    console.log(error);
+});
+
+app.use(Session({
+    secret: 'bhnvx',
+    resave: false,
+    saveUninitialized: true,
+    rolling: true,
+    cookie: {maxAge: 1000*60*60},
+    store: store
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,21 @@ const { render } = require('ejs');
 const express = require('express');
 const router = express.Router();
 
+const crypto = requrie("crypto");
+
 const User = require("../models/user");
 const mongoose = require("mongoose");
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+passport.serializeUser(function(user, done){
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done){
+    done(null, user);
+});
 
 router.get('/', (req, res) => res.render('index'));
 router.get('/login', (req, res) => res.render('login', {page:'login'}));
@@ -23,7 +36,7 @@ router.post("/signup", (req, res, next) => {
                     _id: new mongoose.Types.ObjectId(),
                     name:req.body.name,
                     email: req.body.email,
-                    password: req.body.password
+                    password: crypto.createHash('sha512').update(req.body.password).digest('base64')
                 });
                 user
                     .save()
@@ -38,3 +51,31 @@ router.post("/signup", (req, res, next) => {
         });
 });
 
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+},
+function(req, email, password, done){
+    User.findOne({email: email, password: cryto.createHash('sha512').update(password).digset('base64')}, function(err, user){
+        if(err){
+            throw err;
+        } else if (!user){
+            return done(null, false, req.flash('login_message', '이메일 또는 비밀번호를 확인하세요.'));
+        } else {
+            return done(null, user)
+        }
+    })
+}))
+
+router.post('/login', passport.authenticate('local', {failureRedirect: '/login', failureFlash: true}),
+    function(req, res){
+        res.redirect('/');
+});
+
+router.get("/login", (req, res) => res.render("login", {message: req.flash('login_message')}));
+
+router.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
+});
