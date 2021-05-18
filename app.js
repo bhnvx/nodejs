073f41907cf8133b33810f2e67261a91de93794a
bhnvx <xzt______@naver.com>
2021-05-18 17:26:11 +0900

    Node Test Version

--- app.js
+++ app.js
@@ -1,50 +1,35 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var app = express();
 
-const mongoose = require('mongoose');
-let url = "mongodb://localhost:27017/bhnvx";
-mongoose.connect(url, {useNewUrlParser:true});
-
-const indexRoute = require("./routes/index");
-
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
-
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const Session = require('express-session');
-const flash = require('connect-flash');
-var MongoDBStore = require('connect-mongodb-session')(Session);
-
-app.use(flash());
-
-var store = new MongoDBStore({
-    url: url,
-    collection: 'sessions'
+// DB Setting
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(process.env.MONGO_DB);
+var db = mongoose.connection;
+db.once('open', function(){
+    console.log('DB Connected!');
+});
+db.on('error', function(err){
+    console.log('Error! : ',err);
 });
 
-store.on('error', function(error){
-    console.log(error);
-});
-
-app.use(Session({
-    secret: 'bhnvx',
-    resave: false,
-    saveUninitialized: true,
-    rolling: true,
-    cookie: {maxAge: 1000*60*60},
-    store: store
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.set('views', __dirname + '/views');
+// Setting
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
 
-app.use('/', indexRoute);
+// Route
+app.use('/', require('./routes/index'));
 
-app.listen(PORT, function(){
-    console.log('Example app listening on port', PORT);
+// Port Setting
+var port = 3000;
+app.listen(port, function(){
+    console.log('Server is Connect! http://localhost:'+port);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,86 +1,13 @@
-const { render } = require('ejs');
-const express = require('express');
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
 
-var app = express();
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/node_midules/jquery/dist'));
-
-const crypto = require('crypto');
-
-const User = require("../models/user");
-const mongoose = require("mongoose");
-
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const exp = require('constants');
-
-passport.serializeUser(function(user, done){
-    done(null, user);
+// index Page Setting
+router.get('/', function(req, res){
+    res.render('index');
 });
 
-passport.deserializeUser(function(user, done){
-    done(null, user);
+router.get('/about', function(req, res){
+    res.render('about');
 });
-
-router.get('/', (req, res) => res.render('index'));
-router.get('/login', (req, res) => res.render('login', {page:'login'}));
-router.get('/signup', (req, res) => res.render('signup', {page:'signup'}));
 
 module.exports = router;
-
-router.post("/signup", (req, res, next) => {
-    console.log(req.body);
-    User.find({ email:req.body.email })
-        .exec()
-        .then(user => {
-            if (user.length >= 1) {
-                res.send('<script type="text/javascript">alert("이미 존재하는 이메일입니다."); window.location="/signup"; </script>');
-            } else {
-                const user = new User({
-                    _id: new mongoose.Types.ObjectId(),
-                    name:req.body.name,
-                    email: req.body.email,
-                    password: crypto.createHash('sha512').update(req.body.password).digest('base64')
-                });
-                user
-                    .save()
-                    .then(result => {
-                        console.log(result);
-                        res.redirect("/");
-                    })
-                    .catch(err => {
-                        console.log(err);
-                    });
-                  }
-        });
-});
-
-passport.use(new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password',
-    passReqToCallback: true
-},
-function(req, email, password, done){
-    User.findOne({email: email, password: crypto.createHash('sha512').update(password).digest('base64')}, function(err, user){
-        if(err){
-            throw err;
-        } else if (!user){
-            return done(null, false, req.flash('login_message', '이메일 또는 비밀번호를 확인하세요.'));
-        } else {
-            return done(null, user)
-        }
-    })
-}))
-
-router.post('/login', passport.authenticate('local', {failureRedirect: '/login', failureFlash: true}),
-    function(req, res){
-        res.redirect('/');
-});
-
-router.get("/login", (req, res) => res.render("login", {message: req.flash('login_message')}));
-
-router.get('/logout', function (req, res) {
-    req.logout();
-    res.redirect('/');
-});
